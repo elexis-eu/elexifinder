ed5859d34635ef483a03fb3075956b4da4507f63
David Lindemann <34068491+dlindem@users.noreply.github.com>
2020-06-14 16:17:51 +0200

    ris to ris-json converter

---
+++ python_scripts/ris2json.py
@@ -0,0 +1,35 @@
+# converts RIS into JSON, by dlindem
+import re
+import json
+
+ris_input_file = 'D:/xxx.ris'
+json_output_file = 'D:/yyy.json'
+
+with open(ris_input_file, 'r', encoding="utf-8") as risfile:
+    ris = risfile.read()
+
+risjson = []
+risobjects = ris.split('\nER  - \n')
+for risobject in risobjects:
+    risdict = {}
+    rislines = risobject.split('\n')
+    for risline in rislines:
+        try:
+            splitline = risline.split('  - ',1)
+            riskey = splitline[0]
+            risvalue = splitline[1]
+            if riskey in risdict:
+                risdict[riskey].append(risvalue)
+            else:
+                risdict[riskey] = [risvalue]
+        except IndexError:
+            pass
+        #risdict['ER']=''
+        #    riskey = "ER"
+        #    risvalue = ""
+        #print(riskey+risvalue)
+    risjson.append(risdict)
+print(risjson)
+
+with open(json_output_file, 'w', encoding="utf-8") as json_file:
+	json.dump(risjson, json_file, ensure_ascii=False, indent=2)
