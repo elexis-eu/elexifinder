17c7590fd9c96ddc4a25b9f976583e163fc0ec02
David Lindemann <34068491+dlindem@users.noreply.github.com>
2020-06-14 16:16:24 +0200

    ris-json to ris converter

---
+++ python_scripts/json2ris.py
@@ -0,0 +1,17 @@
+#converts json to ris (json key to ris key, json value to ris value), by dlindem
+import json
+
+risjson_input_file = 'D:/xxx.json'
+ris_result_file = 'D:/yyy.ris'
+
+with open(risjson_input_file, encoding="utf-8") as f:
+	jsonfile =  json.load(f, encoding="utf-8")
+
+print(jsonfile)
+
+with open(ris_result_file, 'w', encoding="utf-8") as risfile:
+	for item in jsonfile:
+		for key, valuelist in item.items():
+			for value in valuelist:
+				risfile.write(key+"  - "+value+"\n")
+		risfile.write("ER  - \n\n")
