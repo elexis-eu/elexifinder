800ee22392e77bcb1829e598a83c0e19daf1f1fe
David Lindemann <34068491+dlindem@users.noreply.github.com>
2020-07-01 21:41:45 +0200

    Update wikipedia2wikidata.py
    Now better scalable (runs api query only once per different Wikipedia page title)

--- python_scripts/wikipedia2wikidata.py
+++ python_scripts/wikipedia2wikidata.py
@@ -12,27 +12,42 @@ with open(zotero_lexbib_rdf_export_file, 'r', encoding="utf-8") as infile:
 
 print(exportlines)
 
+wikipairs = {}
+
 with open(os.path.splitext(zotero_lexbib_rdf_export_file)[0]+"_wikidata.rdf", 'w', encoding="utf-8") as outfile:
     for line in exportlines:
         aulocmatch = re.search('([^<]*)<lexdo:firstAuLoc>https?://en.wikipedia.org/wiki/([^<]+)</lexdo:firstAuLoc>', line)
         arlocmatch = re.search('([^<]*)<lexdo:articleLoc>https?://en.wikipedia.org/wiki/([^<]+)</lexdo:articleLoc>', line)
         if aulocmatch != None:
-            print(aulocmatch.group(2))
-            wdjsonsource = requests.get(url='https://www.wikidata.org/w/api.php?action=wbgetentities&sites=enwiki&format=json&titles='+aulocmatch.group(2))
-            wdjson =  wdjsonsource.json()
-            entities = wdjson['entities']
-            for wdid in entities:
-                print(wdid)
+            wppage = (aulocmatch.group(2))
+            if wppage not in wikipairs:
+                wdjsonsource = requests.get(url='https://www.wikidata.org/w/api.php?action=wbgetentities&sites=enwiki&format=json&titles='+wppage)
+                wdjson =  wdjsonsource.json()
+                entities = wdjson['entities']
+                for wdid in entities:
+                    print("found new wdid "+wdid+" for AUTHORLOC "+wppage)
+                    line = aulocmatch.group(1)+'<lexdo:firstAuLoc>http://wikidata.org/entity/'+wdid+'</lexdo:firstAuLoc>\n'
+                    #outfile.write(line)
+                    wikipairs[wppage] = wdid
+            else:
+                wdid = wikipairs[wppage]
                 line = aulocmatch.group(1)+'<lexdo:firstAuLoc>http://wikidata.org/entity/'+wdid+'</lexdo:firstAuLoc>\n'
-                outfile.write(line)
-        elif aulocmatch == None and arlocmatch != None:
-            print(arlocmatch.group(2))
-            wdjsonsource = requests.get(url='https://www.wikidata.org/w/api.php?action=wbgetentities&sites=enwiki&format=json&titles='+arlocmatch.group(2))
-            wdjson =  wdjsonsource.json()
-            entities = wdjson['entities']
-            for wdid in entities:
-                print(wdid)
-                line = arlocmatch.group(1)+'<lexdo:articleLoc>http://wikidata.org/entity/'+wdid+'</lexdo:articleLoc>\n'
-                outfile.write(line)
-        else:
-            outfile.write(line)
+                print("used known wdid "+wdid+" for AUTHORLOC "+wppage)
+        if arlocmatch != None:
+            #print(arlocmatch.group(2))
+            wppage = (arlocmatch.group(2))
+            if wppage not in wikipairs:
+                wdjsonsource = requests.get(url='https://www.wikidata.org/w/api.php?action=wbgetentities&sites=enwiki&format=json&titles='+wppage)
+                wdjson =  wdjsonsource.json()
+                entities = wdjson['entities']
+                for wdid in entities:
+                    print("found new wdid "+wdid+" for ARTICLELOC "+wppage)
+                    line = arlocmatch.group(1)+'<lexdo:articleLoc>http://wikidata.org/entity/'+wdid+'</lexdo:articleLoc>\n'
+                    #outfile.write(line)
+                    wikipairs[wppage] = wdid
+                    line = arlocmatch.group(1)+'<lexdo:articleLoc>http://wikidata.org/entity/'+wdid+'</lexdo:articleLoc>\n'
+            else:
+                wdid = wikipairs[wppage]
+                print("used known wdid " +wdid+" for ARTICLELOC "+wppage)
+
+        outfile.write(line)
