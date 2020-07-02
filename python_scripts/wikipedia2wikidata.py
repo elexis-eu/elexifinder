d7b6a2b4246ac94ef02635b4bb2d0f0ff9e95f79
David Lindemann <34068491+dlindem@users.noreply.github.com>
2020-07-02 22:28:44 +0200

    Places: wikidata enrichment
    Now produces a csv with lexdo:Place instances, so that only unknown places are sent to wikidata for enrichment

--- python_scripts/wikipedia2wikidata.py
+++ python_scripts/wikipedia2wikidata.py
@@ -4,15 +4,21 @@ import os
 import requests
 import wget
 import json
+import csv
+from SPARQLWrapper import SPARQLWrapper, JSON
 
-zotero_lexbib_rdf_export_file = 'D:/LexBib/exports/test.rdf'
+zotero_lexbib_rdf_export_file = 'D:/LexBib/exports/elex-2009-2017.rdf'
 
 with open(zotero_lexbib_rdf_export_file, 'r', encoding="utf-8") as infile:
     exportlines = infile.readlines()
 
-print(exportlines)
+#print(exportlines)
 
-wikipairs = {}
+try:
+    with open('D:/LexBib/wppage-wdid-mappings.json', encoding="utf-8") as f:
+    	wikipairs =  json.load(f, encoding="utf-8")
+except:
+    wikipairs = {}
 
 with open(os.path.splitext(zotero_lexbib_rdf_export_file)[0]+"_wikidata.rdf", 'w', encoding="utf-8") as outfile:
     for line in exportlines:
@@ -26,12 +32,11 @@ with open(os.path.splitext(zotero_lexbib_rdf_export_file)[0]+"_wikidata.rdf", 'w
                 entities = wdjson['entities']
                 for wdid in entities:
                     print("found new wdid "+wdid+" for AUTHORLOC "+wppage)
-                    line = aulocmatch.group(1)+'<lexdo:firstAuLoc>http://wikidata.org/entity/'+wdid+'</lexdo:firstAuLoc>\n'
-                    #outfile.write(line)
+                    line = aulocmatch.group(1)+'<lexdo:firstAuLoc rdf:resource="http://wikidata.org/entity/'+wdid+'"/>\n'
                     wikipairs[wppage] = wdid
             else:
                 wdid = wikipairs[wppage]
-                line = aulocmatch.group(1)+'<lexdo:firstAuLoc>http://wikidata.org/entity/'+wdid+'</lexdo:firstAuLoc>\n'
+                line = aulocmatch.group(1)+'<lexdo:firstAuLoc rdf:resource="http://wikidata.org/entity/'+wdid+'"/>\n'
                 print("used known wdid "+wdid+" for AUTHORLOC "+wppage)
         if arlocmatch != None:
             #print(arlocmatch.group(2))
@@ -42,12 +47,72 @@ with open(os.path.splitext(zotero_lexbib_rdf_export_file)[0]+"_wikidata.rdf", 'w
                 entities = wdjson['entities']
                 for wdid in entities:
                     print("found new wdid "+wdid+" for ARTICLELOC "+wppage)
-                    line = arlocmatch.group(1)+'<lexdo:articleLoc>http://wikidata.org/entity/'+wdid+'</lexdo:articleLoc>\n'
-                    #outfile.write(line)
+                    line = arlocmatch.group(1)+'<lexdo:articleLoc rdf:resource="http://wikidata.org/entity/'+wdid+'"/>\n'
                     wikipairs[wppage] = wdid
-                    line = arlocmatch.group(1)+'<lexdo:articleLoc>http://wikidata.org/entity/'+wdid+'</lexdo:articleLoc>\n'
             else:
                 wdid = wikipairs[wppage]
+                line = arlocmatch.group(1)+'<lexdo:articleLoc rdf:resource="http://wikidata.org/entity/'+wdid+'"/>\n'
                 print("used known wdid " +wdid+" for ARTICLELOC "+wppage)
 
         outfile.write(line)
+
+# save known mappings to file
+with open('D:/LexBib/wppage-wdid-mappings.json', 'w', encoding="utf-8") as json_file:
+	json.dump(wikipairs, json_file, ensure_ascii=False, indent=2)
+
+# update lexplaces.json
+try:
+    with open('D:/LexBib/lexplaces.json', encoding="utf-8") as lexplacefile:
+        lpdict =  json.load(lexplacefile, encoding="utf-8")
+        print("loaded lexplacefile")
+except:
+    lpdict = {}
+    print("created new lexplacefile")
+wdquerycount = 0
+for mapping in wikipairs:
+    if wikipairs[mapping] not in lpdict:
+        print(mapping+" "+wikipairs[mapping]+" not found in lexplacefile, will query wikidata")
+        wdid = wikipairs[mapping]
+
+        #query wikidata
+        sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+        sparql.setQuery("""SELECT ?label ?country ?countrylabel
+                    WHERE {
+                        wd:"""+wdid+""" rdfs:label ?label .
+                        FILTER (langMatches( lang(?label), "EN" ) )
+                        wd:"""+wdid+""" wdt:P17 ?country.
+                        ?country rdfs:label ?countrylabel .
+                        FILTER (langMatches( lang(?countrylabel), "EN" ) )
+                        } limit 1""")
+        sparql.setReturnFormat(JSON)
+        wdquerycount = wdquerycount + 1
+        try:
+            wddict = sparql.query().convert()
+            datalist = wddict['results']['bindings']
+            print(datalist[0])
+            countryNode = datalist[0]['country']['value']
+            countrylabel = datalist[0]['countrylabel']['value']
+            citylabel = datalist[0]['label']['value']
+            print(countrylabel+" / "+countryNode+" / "+citylabel)
+            wpurl = "http://en.wikipedia.org/wiki/"+mapping
+            print(wpurl)
+            lpdict[wdid] = {"wpurl":wpurl, "country":countryNode, "countrylabel":countrylabel, "citylabel":citylabel}
+
+        except:
+            print(mapping+" "+wikipairs[mapping]+" not found on wikidata")
+            pass
+    else:
+        print(mapping+" "+wikipairs[mapping]+" found in lexplacefile, no wikidata query needed")
+
+print("\nPerformed "+str(wdquerycount)+" wikidata queries.")
+#print(lpdict)
+with open('D:/LexBib/lexplaces.json', 'w', encoding="utf-8") as json_file:
+	json.dump(lpdict, json_file, ensure_ascii=False, indent=2)
+print("\nlexplacefile json updated, finished.")
+
+with open("D:/LexBib/lexplaces.csv", "w", encoding="utf-8", newline="") as csv_file:
+    f = csv.writer(csv_file, delimiter="\t", lineterminator="\n")
+    f.writerow(["Wikidata URI", "gn:wikipediaArticle", "rdfs:label@en", "schema:containedInPlace", "countryLabel"])
+    for place in lpdict:
+        f.writerow(["http://wikidata.org/entity/"+place, lpdict[place]['wpurl'], lpdict[place]['citylabel'], lpdict[place]['country'], lpdict[place]['countrylabel']])
+print("\nlexplacefile csv updated, finished.")
