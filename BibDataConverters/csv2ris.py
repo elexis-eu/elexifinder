9f3dd62fc999cf3ad1f1f1dd0e599680351affd1
David Lindemann <34068491+dlindem@users.noreply.github.com>
2020-12-23 00:46:11 +0100

    Update csv2ris.py

--- BibDataConverters/csv2ris.py
+++ BibDataConverters/csv2ris.py
@@ -3,14 +3,25 @@ import re
 import json
 import csv
 
-with open('D:/Lab_LexBib/BibMerge/videos_locs.csv', encoding="utf-8") as csvfile: # source file
-    csvdict = csv.DictReader(csvfile, delimiter="\t")
+with open('D:/Lab_LexBib/Euralex2021/titles.csv', encoding="utf-8") as csvfile: # source file
+    csvrows = csv.reader(csvfile, delimiter="\t")
+    csvkeys = next(csvrows)
+    print(csvkeys)
     csvjson = []
-    for itemdict in csvdict:
-        for riskey in itemdict:
-            itemdict[riskey] = [itemdict[riskey]]
+    for row in csvrows:
+        itemdict = {}
+        print(str(row))
+        for col in range(len(row)):
+            if row[col] != "":
+                if csvkeys[col] not in itemdict:
+                    itemdict[csvkeys[col]] = [row[col]]
+                else:
+                    itemdict[csvkeys[col]].append(row[col])
+
+
+
         csvjson.append(itemdict)
 print(csvjson)
 
-with open('D:/Lab_LexBib/BibMerge/videos_locs.json', 'w', encoding="utf-8") as json_file: # result file
+with open('D:/Lab_LexBib/Euralex2021/titles.json', 'w', encoding="utf-8") as json_file: # result file
 	json.dump(csvjson, json_file, ensure_ascii=False, indent=2)
