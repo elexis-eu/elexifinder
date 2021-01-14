6932ccccb24278119e6859b806014dcb8b613efc
David Lindemann <34068491+dlindem@users.noreply.github.com>
2021-01-14 16:22:24 +0100

    Update 5levelterms4er.py

--- python_scripts/5levelterms4er.py
+++ python_scripts/5levelterms4er.py
@@ -6,7 +6,7 @@ from unidecode import unidecode
 
 
 try:
-    with open('D:/LexBib/terms/5_level_terms.json', encoding="utf-8") as f:
+    with open('D:/LexBib/terms/4_level_terms.json', encoding="utf-8") as f:
         subjdict =  json.load(f, encoding="utf-8")
 except:
     print ('Error: file "5_level_terms.json" does not exist.')
@@ -17,10 +17,10 @@ bindings = results['bindings']
 print(bindings)
 
 ertermdic = {}
-
+ertermlist = []
 for item in bindings:
 
-    subjectLabelNorm = unidecode(item['subjectLabel']['value']).lower()
+    subjectLabelNorm = unidecode(item['subjectLabel']['value']).lower().rstrip()
     subjectLabelNorm = re.sub(r'[ \-\']','_',subjectLabelNorm)
     subjectLabelNorm = re.sub(r'__+','_',subjectLabelNorm)
 
@@ -37,10 +37,14 @@ for item in bindings:
     itemdict['er_uri'] += item['subject']['value'].replace('http://lexbib.org/terms#','')
     itemdict['er_label'] += item['subjectLabel']['value'].rstrip()
 
+    ertermlist.append(itemdict)
+
     if subjectLabelNorm not in ertermdic:
         ertermdic[subjectLabelNorm] = [itemdict]
     else:
         ertermdic[subjectLabelNorm].append(itemdict)
 
-with open('D:/LexBib/terms/er_skos_5levels.json', 'w', encoding="utf-8") as json_file:
+with open('D:/LexBib/terms/er_skos_4levels_dic.json', 'w', encoding="utf-8") as json_file:
 	json.dump(ertermdic, json_file, indent=2)
+with open('D:/LexBib/terms/er_skos_4levels_list.json', 'w', encoding="utf-8") as json_file:
+	json.dump(ertermlist, json_file, indent=2)
