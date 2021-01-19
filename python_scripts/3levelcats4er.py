9f769f1b44a182b89c044bf3dc5a668d50545711
David Lindemann <34068491+dlindem@users.noreply.github.com>
2021-01-19 01:05:13 +0100

    Is now stable

--- python_scripts/3levelcats4er.py
+++ python_scripts/3levelcats4er.py
@@ -9,34 +9,73 @@ try:
     with open('D:/LexBib/terms/terms_languages_4er.json', encoding="utf-8") as f:
         subjdict =  json.load(f, encoding="utf-8")
 except:
-    print ('Error: file "5_level_terms.json" does not exist.')
+    print ('Error: input file does not exist.')
     sys.exit()
 
 results = subjdict['results']
 bindings = results['bindings']
-print(bindings)
+#print(bindings)
 
 ertermdic = {}
 ertermlist = []
-for item in bindings:
 
-    subjectLabelNorm = unidecode(item['subjectLabel']['value']).lower().rstrip()
+def wr_broaders(source):
+#    print (str(source))
+    er_uri_append = ""
+    er_label_append = ""
+    er_uri_append += '/'+bindings[source]['2nd_level_broader']['value'].replace('http://lexbib.org/terms#','').replace('http://lexvo.org/id/iso639-3/', 'Lang_')
+    er_label_append += '/'+bindings[source]['2nd_level_broaderLabel']['value'].rstrip().title()
+#    print(er_uri_append)
+    if '3rd_level_broader' in bindings[source]:
+        er_uri_append += '/'+bindings[source]['3rd_level_broader']['value'].replace('http://lexbib.org/terms#','').replace('http://lexvo.org/id/iso639-3/', 'Lang_')
+        er_label_append += '/'+bindings[source]['3rd_level_broaderLabel']['value'].rstrip().title()
+
+
+    return [er_uri_append, er_label_append]
+
+
+for item in range(len(bindings)):
+#    print(str(item))
+
+    subjectLabelNorm = unidecode(bindings[item]['subjectLabel']['value']).lower().rstrip()
     subjectLabelNorm = re.sub(r'[ \-\']','_',subjectLabelNorm)
     subjectLabelNorm = re.sub(r'__+','_',subjectLabelNorm)
 
-    itemdict = {'subject_uri':item['subject']['value'],'subjectLabel':item['subjectLabel']['value'].rstrip(),'er_uri':'Term_Lexicography','er_label':'Lexicography'}
-    if '2nd_level_broader' in item:
-        itemdict['er_uri'] += '/'+item['2nd_level_broader']['value'].replace('http://lexbib.org/terms#','').replace('http://lexvo.org/id/iso639-3/', 'Lang_')
-        itemdict['er_label'] += '/'+item['2nd_level_broaderLabel']['value'].rstrip().title()
-        if '3rd_level_broader' in item:
-            itemdict['er_uri'] += '/'+item['3rd_level_broader']['value'].replace('http://lexbib.org/terms#','').replace('http://lexvo.org/id/iso639-3/', 'Lang_')
-            itemdict['er_label'] += '/'+item['3rd_level_broaderLabel']['value'].rstrip().title()
+
+    if '2nd_level_broader' in bindings[item]:
+        itemdict = {'subject_uri':bindings[item]['subject']['value'],'subjectLabel':bindings[item]['subjectLabel']['value'].rstrip(),'er_uri':'Term_Lexicography','er_label':'Lexicography'}
+    #    print(str(item))
+        addbroad = wr_broaders(item)
+    #    print(addbroad)
+        itemdict['er_uri'] += addbroad[0]
+        itemdict['er_label'] += addbroad[1]
+
+        itemdict['er_uri'] += '/'+bindings[item]['subject']['value'].replace('http://lexbib.org/terms#','').replace('http://lexvo.org/id/iso639-3/', 'Lang_')
+        itemdict['er_label'] += '/'+bindings[item]['subjectLabel']['value'].rstrip().title()
+
+        # else:
+        #     itemdict['er_uri'] += '/'+item['subject']['value'].replace('http://lexbib.org/terms#','').replace('http://lexvo.org/id/iso639-3/', 'Lang_')
+        #     itemdict['er_label'] += '/'+item['subjectLabel']['value'].rstrip().title()
+        #     item += 1
+    elif '2nd_level_broader' not in bindings[item]:
+        if 'synonym' not in bindings[item]:
+            print('Found orphaned term '+subjectLabelNorm)
+            continue
+            #print(bindings[item]['synonym'])
         else:
-            itemdict['er_uri'] += '/'+item['subject']['value'].replace('http://lexbib.org/terms#','').replace('http://lexvo.org/id/iso639-3/', 'Lang_')
-            itemdict['er_label'] += '/'+item['subjectLabel']['value'].rstrip().title()
-    else:
-        itemdict['er_uri'] += '/'+item['subject']['value'].replace('http://lexbib.org/terms#','').replace('http://lexvo.org/id/iso639-3/', 'Lang_')
-        itemdict['er_label'] += '/'+item['subjectLabel']['value'].rstrip().title()
+            for binding in range(len(bindings)):
+                #print (binding['subject']['value'])
+                if bindings[binding]['subject']['value'] == bindings[item]['synonym']['value']:
+                    #print('hallo synonym')
+                    if '2nd_level_broader' in bindings[binding]:
+                        itemdict = {'subject_uri':bindings[item]['subject']['value'],'subjectLabel':bindings[item]['subjectLabel']['value'].rstrip(),'er_uri':'Term_Lexicography','er_label':'Lexicography'}
+                        addbroad = wr_broaders(binding)
+                        itemdict['er_uri'] += addbroad[0]
+                        itemdict['er_label'] += addbroad[1]
+
+                        itemdict['er_uri'] += '/'+bindings[item]['subject']['value'].replace('http://lexbib.org/terms#','').replace('http://lexvo.org/id/iso639-3/', 'Lang_')
+                        itemdict['er_label'] += '/'+bindings[item]['subjectLabel']['value'].rstrip().title()
+
 
 
     # if '4th_level_broader' in item:
@@ -48,12 +87,15 @@ for item in bindings:
     #itemdict['er_label'] += item['subjectLabel']['value'].rstrip()
     #itemdict['er_uri'] += item['subject']['value'].replace('http://lexbib.org/terms#','')
 
+
     ertermlist.append(itemdict)
 
     if subjectLabelNorm not in ertermdic:
         ertermdic[subjectLabelNorm] = [itemdict]
     else:
         ertermdic[subjectLabelNorm].append(itemdict)
+
+
 
 with open('D:/LexBib/terms/er_skos_3levelcats_dic.json', 'w', encoding="utf-8") as json_file:
 	json.dump(ertermdic, json_file, indent=2)
