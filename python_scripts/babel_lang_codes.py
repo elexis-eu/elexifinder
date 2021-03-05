3ca833a26845c587e2366c1196337ecb5d1404e6
David Lindemann <34068491+dlindem@users.noreply.github.com>
2021-03-05 15:03:27 +0100

    Create babel_lang_codes.py

---
+++ python_scripts/babel_lang_codes.py
@@ -0,0 +1,42 @@
+#This maps iso-639-3 to BabelNet language codes (Elexis languages)
+
+langcodemapping = {
+"sqi":"SQ", #Albanian
+"eus":"EU", #Basque
+"bel":"BE", #Belarusian
+"bul":"BG", #Bulgarian
+"cat":"CA", #Catalan
+"hrv":"HR", #Croatian
+"ces":"CS", #Czech
+"dan":"DA", #Danish
+"nld":"NL", #Dutch
+"eng":"EN", #English
+"est":"ET", #Estonian
+"fin":"FI", #Finnish
+"fra":"FR", #French
+"glg":"GL", #Galician
+"deu":"DE", #German
+"ell":"EL", #Greek
+"hun":"HU", #Hungarian
+"isl":"IS", #Icelandic
+"gle":"GA", #Irish
+"ita":"IT", #Italian
+"lav":"LV", #Latvian
+"lit":"LT", #Lithuanian
+"ltz":"LB", #Luxembourgish
+"mkd":"MK", #Macedonian
+"mlt":"MT", #Maltese
+"nob":"NO", #Norwegian Bokmal
+"nno":"NN", #Nynorsk
+"pol":"PL", #Polish
+"por":"PT", #Portuguese
+"ron":"RO", #Romanian
+"rus":"RU", #Russian
+"srp":"SR", #Serbian
+"slk":"SK", #Slovak
+"slv":"SL", #Slovene
+"spa":"ES", #Spanish
+"swe":"SV", #Swedish
+"ukr":"UK", #Ukrainian
+
+}
