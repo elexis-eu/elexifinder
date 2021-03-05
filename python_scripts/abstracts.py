1ea10e2def27a89c61ebb4664eddce310069bae9
David Lindemann <34068491+dlindem@users.noreply.github.com>
2021-03-05 14:55:35 +0100

    Update abstracts.py

--- python_scripts/abstracts.py
+++ python_scripts/abstracts.py
@@ -6,10 +6,10 @@
 #
 # where {
 # 	?uri  :abstract ?abstractnode .
-#     ?uri :zoteroItemUri ?zotitemnode .
-#     OPTIONAL {?zotitemnode zotexport:pdfFile ?pdffiles .}
-#     OPTIONAL{ ?abstractnode :abstractText ?abstracttext . }
-#     OPTIONAL{ ?abstractnode :abstractLanguage ?abstractlanguri .}
+#	 ?uri :zoteroItemUri ?zotitemnode .
+#	 OPTIONAL {?zotitemnode zotexport:pdfFile ?pdffiles .}
+#	 OPTIONAL{ ?abstractnode :abstractText ?abstracttext . }
+#	 OPTIONAL{ ?abstractnode :abstractLanguage ?abstractlanguri .}
 #
 # }
 # GROUP BY ?uri ?pdffiles (STRAFTER( str(?abstractlanguri), "iso639-3/" ) as ?abstractlang) ?abstracttext
@@ -18,21 +18,21 @@ import json, time, re
 
 
 with open('D:/LexBib/abstracts/query-result.srj', 'r', encoding="utf-8") as infile:
-    sparqldict = json.load(infile, encoding="utf-8")['results']['bindings']
+	sparqldict = json.load(infile, encoding="utf-8")['results']['bindings']
 absdict = {}
 for item in sparqldict:
-    abstracttext = ""
-    abstractlang = None
-    if 'uri' in item:
-        uri = item['uri']['value']
-        print(uri)
-    if 'abstracttext' in item:
-        abstracttext = item['abstracttext']['value']
-    if 'abstractlang' in item:
-        abstractlang = item['abstractlang']['value']
-    if abstracttext != "":
-        absdict[uri]={'text':abstracttext, 'lang':abstractlang}
+	abstracttext = ""
+	abstractlang = None
+	if 'uri' in item:
+		uri = item['uri']['value']
+		print(uri)
+	if 'abstracttext' in item:
+		abstracttext = item['abstracttext']['value']
+	if 'abstractlang' in item:
+		abstractlang = item['abstractlang']['value']
+	if abstracttext != "":
+		absdict[uri]={'text':abstracttext, 'lang':abstractlang}
 
 
 with open('D:/LexBib/abstracts/abstracts.json', 'w', encoding="utf-8") as outfile:
-    json.dump(absdict, outfile, ensure_ascii=False, indent=2)
+	json.dump(absdict, outfile, ensure_ascii=False, indent=2)
