c599982feaba82a13161d532bdbeb2bc11a02193
David Lindemann <34068491+dlindem@users.noreply.github.com>
2021-03-10 22:00:20 +0100

    Version successfully used for first test batch

--- python_scripts/babelterms.py
+++ python_scripts/babelterms.py
@@ -2,57 +2,32 @@ import csv
 import json
 import requests
 import time
+import babel_lang_codes
 
 def getbabeltrans(bnid):
-	languages = {
-	"SL":"slv",
-	"ES":"spa",
-	"IT":"ita",
-	"DE":"deu",
-	"PT":"por",
-	"CS":"ces",
-	"ET":"est",
-	"HU":"hun",
-	"DA":"dan",
-	"SV":"swe",
-	"NO":"nor",
-	"FI":"fin",
-	"HR":"hrv",
-	"SR":"srp",
-	"LV":"lav",
-	"SK":"slk",
-	"PL":"pol",
-	"BG":"bul",
-	"RO":"ron",
-	"IS":"isl",
-	"BE":"bel",
-	"GA":"gle",
-	#"":"cnr", MONTENEGRIN NOT ON BABELNET
-	"MK":"mkd",
-	"RU":"rus",
-	"UK":"ukr",
-	"EU":"eus"
-	}
 
 	translations = {}
-	for language in languages:
-		response = requests.get("https://babelnet.io/v6/getSynset?id="+bnid+"&targetLang="+language+"&key=2ced12ea-bd40-4ee4-9ad4-4d054c9d48e2", headers={'Accept-Encoding': 'gzip'})
+	for language in babel_lang_codes.langcodemapping.keys():
+		babellang = babel_lang_codes.langcodemapping[language]
+		response = requests.get("https://babelnet.io/v6/getSynset?id="+bnid+"&targetLang="+babellang+"&key=2ced12ea-bd40-4ee4-9ad4-4d054c9d48e2", headers={'Accept-Encoding': 'gzip'})
 		respdict_str = response.content.decode("UTF-8")
 		#print(respdict_str)
 		respdict = json.loads(respdict_str)
 		#print(str(respdict))
 		if 'senses' in respdict:
 			if len(respdict['senses']) > 0:
-				translations[languages[language]] = respdict['senses'][0]['properties']['lemma']['lemma']
+				translations[language] = {"response":respdict_str,"lemma":respdict['senses'][0]['properties']['lemma']['lemma']}
+			else:
+				translations[language] = False
 		else:
 			print('Something went wrong with BabelNet API request...')
 			return False
 	return translations
 
 with open('D:/LexBib/terms/babeltranslations.json', 'r', encoding="utf-8") as targetfile:
-	target = json.load(infile)
+	target = json.load(targetfile)
 
-with open('D:/LexBib/terms/term_bnid_conf.csv') as csvfile: # source file
+with open('D:/LexBib/terms/term_bnid_status_labels.csv') as csvfile: # source file
 	termdict = csv.DictReader(csvfile)
 	termlist = list(termdict)
 	print(str(termlist))
@@ -61,19 +36,22 @@ with open('D:/LexBib/terms/term_bnid_conf.csv') as csvfile: # source file
 	count = 0
 	for row in termlist:
 		count +=1
-		print ('Now processing term '+str(count)+' of '+str(totalrows)+': '+row["term_uri"])
-		if row["term_uri"] in target:
+		print ('\nNow processing term '+str(count)+' of '+str(totalrows)+': '+row["term"])
+		if row["term"] in target:
 			print("This term has already got translations from BabelNet, skipped.")
 		else:
-			#print(str(row))
-			target[row["term_uri"]] = {"bn_id":row["bnid"],"conf":row["conf"]}
 			if row["bnid"].startswith("bn:"):
 				translations = getbabeltrans(row["bnid"])
-				if translations = False
+				if translations == False:
 					print("BabelNet does not respond as it should, probably because we have run out of babelcoins")
 					break
 				else:
-					target[row["term_uri"]]["translations"] = translations
+					target[row["term"]] = {"bn_id":row["bnid"],"status":row["status"],"prefLabel":row['prefLabel'],"altLabel":row["altLabel"]}
+					target[row["term"]]["translations"] = translations
+					print('*** OK. Got translations from BabelNet.')
+			else:
+				print('*** No BabelNet ID for this term.')
+				target[row["term"]] = {"bn_id":False,"status":row["status"],"prefLabel":row['prefLabel'],"altLabel":row["altLabel"]}
 
 with open('D:/LexBib/terms/babeltranslations.json', 'w', encoding="utf-8") as targetfile:
 	json.dump(target, targetfile, indent=2)
---
+++ python_scripts/babeltrans2xml.py
@@ -0,0 +1,105 @@
+import xml.etree.ElementTree as xml
+from xml.dom import minidom
+import json
+import babel_lang_codes
+
+with open('D:/LexBib/terms/SKOS4Lexonomy.json', encoding="utf-8") as f:
+	skos =  json.load(f, encoding="utf-8")
+	subjdict = {}
+	for item in skos['results']['bindings']:
+		subjdict[item['subject']['value']] = {
+		"broaders" : item['broaderLabels']['value'].split("@"),
+		"narrowers" : item['narrowerLabels']['value'].split("@"),
+		"exactMatches" : item['exactMatchLabels']['value'].split("@"),
+		"related" : item['relatedLabels']['value'].split("@"),
+		"definitions" : item['definitions']['value'].split("@"),
+		}
+#print(str(subjdict))
+
+with open('D:/LexBib/terms/babeltranslations.json', encoding="utf-8") as f:
+	babeldict =  json.load(f, encoding="utf-8")
+
+root = xml.Element('a3iadrmk')
+processed_prefLabels = [] # this prevents from processing twice homograph term eng prefLabel (assumes that those will be exactMatches)
+for subj in babeldict:
+	if babeldict[subj]['prefLabel'] not in processed_prefLabels:
+		print(subj)
+		entry = xml.Element('entry')
+		root.append(entry)
+		entry.set('lexbib_id',subj)
+		status_entry = xml.SubElement(entry, 'status_entry')
+		term = xml.SubElement(entry, 'term')
+		label_eng = xml.SubElement(term, 'label_eng')
+		label_eng.text = babeldict[subj]['prefLabel']
+		processed_prefLabels.append(babeldict[subj]['prefLabel'])
+		if 'altLabel' in babeldict[subj]:
+			altlabel_eng = xml.SubElement(term, 'altlabel_eng')
+			altlabel_eng.text = babeldict[subj]['altLabel']
+
+		for item in subjdict[subj]['broaders']:
+			if len(item) > 0:
+				broader = xml.SubElement(term, 'broader_eng')
+				broader.text = item
+		for item in subjdict[subj]['narrowers']:
+			if len(item) > 0:
+				narrower = xml.SubElement(term, 'narrower_eng')
+				narrower.text = item
+
+		for item in subjdict[subj]['exactMatches']:
+			if len(item) > 0 and item.lower() != babeldict[subj]['prefLabel'].lower():
+				exactM = xml.SubElement(term, 'exactmatch_eng')
+				exactM.text = item
+
+		for item in subjdict[subj]['related']:
+			if len(item) > 0:
+				related = xml.SubElement(term, 'related_eng')
+				related.text = item
+
+		for item in subjdict[subj]['definitions']:
+			if len(item) > 0:
+				definition = xml.SubElement(term, 'definition_eng')
+				definition.text = item
+
+		if 'bn_id' in babeldict[subj]:
+			Babelnet_ID = xml.SubElement(term, 'Babelnet_ID')
+			Babelnet_ID.text = babeldict[subj]['bn_id']
+			Babelnet_ID.set('match_quality',babeldict[subj]['status'])
+
+		translations = xml.SubElement(entry, 'translations')
+		for lang in babel_lang_codes.langcodemapping.keys():
+			langelementname = "term_"+lang
+			langelement = xml.SubElement(entry, langelementname)
+
+			if 'translations' in babeldict[subj] and babeldict[subj]['translations'][lang] != False:
+				#get HIGH_QUALITY lemmata from Babel synset
+				babelSenses = json.loads(babeldict[subj]['translations'][lang]['response'])['senses']
+				equivs = []
+				for babelSense in babelSenses:
+					if babelSense['properties']['lemma']['type'] == "HIGH_QUALITY":
+						equivs.append(babelSense['properties']['lemma']['lemma'])
+				equivs_uniq = []
+				for equiv in equivs:
+					if equiv not in equivs_uniq:
+						equivs_uniq.append(equiv)
+				equivs_iter = iter(equivs_uniq)
+				langelement.set('status',"AUTOMATIC")
+				label = xml.SubElement(langelement, 'label')
+				try:
+					label.text = next(equivs_iter)
+				except: # if no HIGH_QUALITY lemma is there, take the first listed
+					label.text = babeldict[subj]['translations'][lang]['lemma']
+				while True:
+					try:
+						altEquiv = next(equivs_iter)
+					except:
+						break
+					altlabel = xml.SubElement(langelement, 'altlabel')
+					altlabel.text = altEquiv
+			else:
+				langelement.set('status',"MISSING")
+
+tree_obj = xml.ElementTree(root)
+with open('D:/LexBib/terms/lexonomy_upload.xml', "w", encoding='utf-8') as file:
+	reparsed = minidom.parseString(xml.tostring(root, 'utf-8')).toprettyxml(indent = "\t")
+	#print(reparsed)
+	file.write(reparsed)
