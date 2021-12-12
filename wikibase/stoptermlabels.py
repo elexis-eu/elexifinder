094812914a0fd263c7bb880dd37aa173dc1c50c7
David Lindemann <34068491+dlindem@users.noreply.github.com>
2021-12-12 22:06:08 +0100

    Create stoptermlabels.py

---
+++ wikibase/stoptermlabels.py
@@ -0,0 +1,44 @@
+# list of term labels not to be considered (not to be sent to keyword_processor)
+# These term labels are very ambiguous and bound to produce many false positives
+# (at the moment, English only)
+stoptermlabels = {}
+
+stoptermlabels['eng'] = """example
+case
+aspect
+survey
+voice
+filter
+article
+entry
+number
+context
+usage
+translation
+customization
+innovative feature
+several number
+design
+access
+frequency
+label
+style
+variant
+size
+register
+illustration
+device
+interface
+dual
+modal
+particle
+clarity
+progressive
+note
+person""".split('\n')
+
+stoptermlabels['spa'] = """ejemplo
+caso
+aspecto
+persona
+diverso""".split('\n')
