b3bba83d01e4a6c5d0011b975b6d1710199be69c
David Lindemann <34068491+dlindem@users.noreply.github.com>
2022-01-20 00:39:38 +0100

    Update export-elexifinder.py

--- wikibase/export-elexifinder.py
+++ wikibase/export-elexifinder.py
@@ -9,44 +9,66 @@ import sparql
 import requests
 from datetime import datetime
 import shutil
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
-import sys
+import sys, traceback
 sys.path.insert(1, os.path.realpath(os.path.pardir))
 from elexifinder import collection_images
-import wikify
+import wikify, config
 
 
 # items to export
 
-export_items = """Q9054
-Q13618
-Q10752
-Q13279
-Q12734
-Q8693
-Q12270
-Q11845
-Q15742
-Q11229""".split("\n")
+print('Will now get a list of all bibitems that have P85 Elexifinder collection...')
+
+url = "https://lexbib.elex.is/query/sparql?format=json&query=PREFIX%20lwb%3A%20%3Chttp%3A%2F%2Flexbib.elex.is%2Fentity%2F%3E%0APREFIX%20ldp%3A%20%3Chttp%3A%2F%2Flexbib.elex.is%2Fprop%2Fdirect%2F%3E%0APREFIX%20lp%3A%20%3Chttp%3A%2F%2Flexbib.elex.is%2Fprop%2F%3E%0APREFIX%20lps%3A%20%3Chttp%3A%2F%2Flexbib.elex.is%2Fprop%2Fstatement%2F%3E%0APREFIX%20lpq%3A%20%3Chttp%3A%2F%2Flexbib.elex.is%2Fprop%2Fqualifier%2F%3E%0APREFIX%20lpr%3A%20%3Chttp%3A%2F%2Flexbib.elex.is%2Fprop%2Freference%2F%3E%0APREFIX%20lno%3A%20%3Chttp%3A%2F%2Flexbib.elex.is%2Fprop%2Fnovalue%2F%3E%0A%0Aselect%20%3FbibItem%0A%0Awhere%20%7B%0A%20%20%3FbibItem%20ldp%3AP85%20%3Fcollection%20.%0A%20%20%7D"
+done = False
+while (not done):
+	try:
+		r = requests.get(url)
+		bindings = r.json()['results']['bindings']
+	except Exception as ex:
+		print('Error: SPARQL request failed: '+str(ex))
+		time.sleep(2)
+		continue
+	done = True
+#print(str(bindings))
+
+export_items = []
+for binding in bindings:
+	export_items.append(binding['bibItem']['value'].replace("http://lexbib.elex.is/entity/",""))
+print('Found '+str(len(export_items))+' items.\n')
+time.sleep(1)
+
+
+# export_items = """Q6059
+# Q15467
+# Q9054
+# Q13618
+# Q10752
+# Q13279
+# Q12734
+# Q8693
+# Q12270
+# Q11845
+# Q15742
+# Q11229""".split("\n")
 
 # pubTime
 pubTime = str(datetime.now()).replace(' ','T')[0:22]
 print(pubTime)
 
 # Elexifinder API key
-with open('D:/LexBib/elexifinder/elexifinder_api_key.txt') as pwdfile:
+with open(config.datafolder+'elexifinder/elexifinder_api_key.txt') as pwdfile:
 	EFapiKey = pwdfile.read()
 # Event Registry "news api" key
-with open('D:/LexBib/elexifinder/eventregistry_api_key.txt') as pwdfile:
+with open(config.datafolder+'elexifinder/eventregistry_api_key.txt') as pwdfile:
 	ERapiKey = pwdfile.read()
-# with open('D:/LexBib/bodytxt/foundwikiterms.json', "r", encoding="utf-8") as jsonfile:
+# with open(config.datafolder+'bodytxt/foundwikiterms.json', "r", encoding="utf-8") as jsonfile:
 # 	wikiterms = json.load(jsonfile)
-wikidir = os.listdir('D:/LexBib/bodytxt/wikification')
+wikidir = os.listdir(config.datafolder+'bodytxt/wikification')
 EFhost = "http://finder.elex.is"
 
 # get donelist
-with open('D:/LexBib/elexifinder/export_last_donelist.txt', 'r', encoding="utf-8") as txt_file:
+with open(config.datafolder+'elexifinder/export_last_donelist.txt', 'r', encoding="utf-8") as txt_file:
 	donelist = txt_file.read().split("\n")
 
 def get_item_data(itemuri):
@@ -97,7 +119,7 @@ where {
   ?zotstatement lps:P16 ?zotItem.
 #  OPTIONAL {?zotstatement lpq:P70 ?pdffile .}
 #  OPTIONAL {?zotstatement lpq:P71 ?txtfile .}
-  OPTIONAL {?bibItem ldp:P21|ldp:P44 ?fullTextUrls .}
+  OPTIONAL {?bibItem ldp:P21|ldp:P113|ldp:P112 ?fullTextUrls .}
   OPTIONAL {?bibItem ldp:P29 ?auLocUri . ?auLocUri ldp:P66 ?authorLoc .}
   OPTIONAL {?bibItem ldp:P9 ?containerUri .
   OPTIONAL {?containerUri ldp:P21|ldp:P111 ?containerFullTextUrl .}
@@ -139,7 +161,7 @@ limit 10
 		return itemdata
 
 # load subject list
-with open('D:/LexBib/terms/elexifinder-catlabels-3level.csv', encoding="utf-8") as csvfile:
+with open(config.datafolder+'terms/elexifinder-catlabels-3level.csv', encoding="utf-8") as csvfile:
 	catlabels_csv = csv.DictReader(csvfile)
 	termlabels = {}
 	catlabels = {}
@@ -152,10 +174,10 @@ with open('D:/LexBib/terms/elexifinder-catlabels-3level.csv', encoding="utf-8") 
 
 #print(str(catlabels))
 
-with open('D:/LexBib/bodytxt/foundterms_last.json', encoding="utf-8") as jsonfile:
+with open(config.datafolder+'bodytxt/foundterms_last.json', encoding="utf-8") as jsonfile:
 	foundterms = json.load(jsonfile)
 
-with open('D:/LexBib/bodytxt/bodytxt_collection.json', encoding="utf-8") as jsonfile:
+with open(config.datafolder+'bodytxt/bodytxt_collection.json', encoding="utf-8") as jsonfile:
 	bodytxts = json.load(jsonfile)
 
 elexifinder = []
@@ -163,31 +185,33 @@ itemcount = 0
 
 for itemuri in export_items:
 
+	itemcount += 1
+	print('\nNow processing item '+str(itemcount)+' of '+str(len(export_items))+': '+itemuri)
+
 	if itemuri in donelist:
 		print(itemuri+" is in donelist, skipped.")
 		continue
 
 	conceptlabels = []
-	itemcount += 1
-	target = {}
-	target['uri'] = itemuri
-	print('\n['+str(itemcount)+'] '+itemuri)
+	target = {'uri': itemuri}
 
 	# get term indexation
 	target['concepts'] = []
 	target['categories'] = []
 	if itemuri not in foundterms:
-		print('No term indexation found for item '+itemuri)
+		print('Term indexation not found for item '+itemuri)
 		time.sleep(1)
 	else:
+		print('Found lexvoc term indexation data.')
 		for foundterm in foundterms[itemuri]:
-			target['concepts'].append({
-			"uri": "lwb:"+foundterm,
-			"label": termlabels[foundterm],
-			"type": "person",
-			"wgt": 1
-			})
-			conceptlabels.append(termlabels[foundterm].lower())
+			if termlabels[foundterm] not in conceptlabels: # avoids double listing of homograph term labels
+				target['concepts'].append({
+				"uri": "lexbib:"+foundterm,
+				"label": termlabels[foundterm],
+				"type": "person",
+				"wgt": 1
+				})
+				conceptlabels.append(termlabels[foundterm].lower())
 			if foundterm not in catlabels:
 				print('er-catlabel not found. Must be a redirect uri.')
 				time.sleep(1)
@@ -213,24 +237,33 @@ for itemuri in export_items:
 	# target['crawlTm'] = item['modTM']['value'][0:22]
 	zotItemUri = "https://www.zotero.org/groups/1892855/lexbib/items/"+itemdata[5]+"/item-details"
 	target['details']['zotItemUri'] = zotItemUri
-	target['url'] = itemdata[7]
+	if itemdata[7]:
+		target['url'] = itemdata[7]
+	else:
+		print("Download/access URL missing.")
 	collection = int(itemdata[2])
 	target['details']['collection'] = collection
 	#target['images']="https://raw.githubusercontent.com/elexis-eu/elexifinder/master/elexifinder/collection-images/collection_"+str(collection)+".jpg"
-	target['images'] = collection_images.images[collection]
-	print('Found image: '+target['images'])
+	target['images'] = [collection_images.images[collection]]
+	print('Found image: '+target['images'][0])
 #	if 'container' in item:
 #		target['sourceUri'] = item['container']['value'] # replaced by containerFullTextUrl or containerUri
 	# if 'containerFullTextUrl' in item:
 	# 	target['sourceUri'] = item['containerFullTextUrl']['value']
 	# 	target['url'] = item['containerFullTextUrl']['value'] # this overwrites zotero item in target['url']
 	# elif 'containerUri' in item:
-	target['sourceUri'] = itemdata[8]
+	if itemdata[8]:
+		target['sourceUri'] = itemdata[8]
+	else:
+		print('Container URI missing.')
 	# 	target['url'] = item['containerUri']['value'] # this overwrites zotero item in target['url']
 	# if 'sourceUri' not in target:
 	# 	print ('*** ERROR: nothing to use as sourceUri in '+itemuri+'!')
 	# 	problemlog.append('*** ERROR: nothing to use as sourceUri in '+itemuri+'!\n')
-	target['sourceTitle'] = itemdata[9]
+	if itemdata[9]:
+		target['sourceTitle'] = itemdata[9]
+	else:
+		print('Container Title missing.')
 
 	target['lang'] = itemdata[6]
 	if itemdata[11]:
@@ -244,7 +277,7 @@ for itemuri in export_items:
 		target['sourceCountry'] = itemdata[13] # item['articleCountryLabel']['value']
 	if itemdata[10]:
 		target['locationUri'] = itemdata[10]  # item['authorLoc']['value']
-	if itemdata[7]: # this overwrites zotero item URL in target['url']
+	if itemdata[7] != None: # this overwrites zotero item URL in target['url']
 		target['url'] = itemdata[7] # item['fullTextUrl']['value']
 	if itemdata[14] == "Q30":
 		target['type'] = "video"
@@ -255,24 +288,26 @@ for itemuri in export_items:
 		print('Textbody found and copied.')
 		if itemuri+".json" in wikidir:
 			print('Found wikification result file for '+itemuri)
-			with open('D:/LexBib/bodytxt/wikification/'+itemuri+'.json', 'r', encoding="utf-8") as jsonfile:
+			with open(config.datafolder+'bodytxt/wikification/'+itemuri+'.json', 'r', encoding="utf-8") as jsonfile:
 				wikiconcepts = json.load(jsonfile)
 		else:
+			print('Wikification still missing, will now be done for item '+itemuri)
 			wikiconcepts = wikify.wikify(itemuri, target['body'])
 		for wikiconcept in wikiconcepts['concepts']:
-			if wikiconcept['label'].lower() not in conceptlabels:
+			wikiconcept['label'] = wikiconcept['label'].lower() # convert wikipage title to lowercase
+			wikiconcept['uri'] = re.sub(r'^wd:Q', 'wikidata:Q', wikiconcept['uri']) # wikify.py returns "wd:" prefix for wikidata items
+			if wikiconcept['label'] not in conceptlabels:
 				if wikiconcept['type'] == "person":
 					print('Skipped wikiperson '+wikiconcept['label'])
 				else:
 					target['concepts'].append(wikiconcept)
 		print('Wikification successful.')
-		with open('D:/LexBib/elexifinder/er_api/er_answer_'+itemuri+'.json', "w", encoding="utf-8") as jsonfile:
-			json.dump({'concepts':target['concepts']}, jsonfile, indent=2)
 	else:
-		print('No textbody found.')
+		print('Textbody missing for this item.')
 
 # upload to elexifinder
 	print('Will upload BibItem '+itemuri+' to Elexifinder.')
+	#print(str(target))
 	addParams = {
 	    "article": [json.dumps(target)],
 	    "apiKey": EFapiKey
@@ -283,7 +318,10 @@ for itemuri in export_items:
 		attempts += 1
 		if attempts > 4:
 			print('Elexifinder update failed 5 times. Abort.')
-			sys.exit()
+			with open(config.datafolder+'elexifinder/upload-errors.txt', 'a', encoding="utf-8") as errorfile:
+				errorfile.write(itemuri+"\t"+str(datetime.now()).replace(' ','T')[0:22]+"\t"+str(errortext)+"\n")
+			#sys.exit()
+			break
 		res = requests.post(EFhost + "/api/admin/v1/addArticle", json = addParams)
 		print(res.json()['info'])
 		if "Added 0 new articles" in res.json()['info']:
@@ -300,15 +338,20 @@ for itemuri in export_items:
 			else:
 				print('Elexifinder upload failed. Will try again. Error info was:')
 				print(errortext)
+
+				print(str(res.json()))
 				time.sleep(1)
 		else:
 			done = True
 	#print('Uploaded item '+itemuri+' to Elexifinder.')
-	donelist.append(itemuri)
-	elexifinder.append(target)
-	print('Finished item '+itemuri+'.')
-	with open('D:/LexBib/elexifinder/export_last_donelist.txt', 'a', encoding="utf-8") as txt_file:
-		txt_file.write(itemuri+'\n')
+	if done:
+		donelist.append(itemuri)
+		elexifinder.append(target)
+		print('Finished item '+itemuri+'.')
+		with open(config.datafolder+'elexifinder/export_last_donelist.txt', 'a', encoding="utf-8") as txt_file:
+			txt_file.write(itemuri+'\n')
+	else:
+		print('Item could not be uploaded. Listed in upload_errors.txt')
 
 # end of item loop
 
@@ -321,20 +364,9 @@ elexidict = {}
 for item in elexifinder:
 	elexidict[item['uri']] = item
 
-print('Will now update Elexifinder suggest indices...')
-res = requests.get(EFhost + "/api/v1/suggestConcepts", { "action": "updatePrefixes" })
-print(str(res))
-time.sleep(1)
-res = requests.get(EFhost + "/api/v1/suggestCategories", { "action": "updatePrefixes" })
-print(str(res))
-time.sleep(1)
-res = requests.get(EFhost + "/api/v1/suggestSources", { "action": "updatePrefixes" })
-print(str(res))
-time.sleep(1)
-res = requests.get(EFhost + "/api/v1/suggestAuthors", { "action": "updatePrefixes" })
-print(str(res))
+wikify.update_elexifinder_indices()
 
 
-with open('D:/LexBib/elexifinder/export_last.json', 'w', encoding="utf-8") as json_file: # path to result JSON file
+with open(config.datafolder+'elexifinder/export_last.json', 'w', encoding="utf-8") as json_file: # path to result JSON file
 	json.dump(elexidict, json_file, indent=2)
 	print('\n=============================================\nCreated upload summary JSON file "export_last.json"... Finished.\n')
