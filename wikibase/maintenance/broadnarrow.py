94fa8ebec193cca6a8122ff289a2684f7b77a1db
David Lindemann <34068491+dlindem@users.noreply.github.com>
2022-04-05 00:47:20 +0200

    Update broadnarrow.py

--- wikibase/maintenance/broadnarrow.py
+++ wikibase/maintenance/broadnarrow.py
@@ -8,12 +8,13 @@ import sys
 import os
 import sparql
 import json
+
 sys.path.insert(1, os.path.realpath(os.path.pardir))
 import lwb
 import config
 
 print('(1) get broader-related concepts where the inverse narrow relation is not present...\n')
-query = config.lwb_prefixes+"""
+query = config.lwb_prefixes + """
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
 select distinct ?n ?nLabel ?b ?bLabel (REPLACE(STR(?redirect),".*Q","Q") AS ?redirect_qid)
 
@@ -39,40 +40,42 @@ print(query)
 
 url = "https://lexbib.elex.is/query/sparql"
 print("Waiting for SPARQL...")
-sparqlresults = sparql.query(url,query)
+sparqlresults = sparql.query(url, query)
 print('\nGot term list from LexBib SPARQL.')
 
-#go through sparqlresults
+# go through sparqlresults
 rowindex = 0
 
 for row in sparqlresults:
-	rowindex += 1
-	item = sparql.unpack_row(row, convert=None, convert_type={})
-	print('\nNow processing item ['+str(rowindex)+']...')
-	print(str(item))
-	narrower_uri = item[0].replace("http://lexbib.elex.is/entity/","")
-	narrower_label = item[1]
-	broader_uri = item[2].replace("http://lexbib.elex.is/entity/","")
-	broader_label = item[3]
-	redirect_qid = item[4]
-	if redirect_qid != None: # broader-rel points to redirect item
-		if redirect_qid.startswith("Q"):
-			print('Updating '+broader_uri+' redirect with '+redirect_qid+'...')
-			broaderclaims = lwb.getclaims(narrower_uri,"P72")[1]
-			if "P72" in broaderclaims:
-				broaderstatement = broaderclaims['P72'][0]['id']
-				lwb.setclaimvalue(broaderstatement, redirect_qid, "item")
-				print('Updating relation: '+narrower_uri+' "'+str(broader_label)+'" (skos:narrower) "'+str(narrower_label)+'"...')
-				lwb.updateclaim(redirect_qid,"P73",narrower_uri,"item")
-		else:
-			print('Strange: for '+broader_uri+' I got redirect: '+redirect_qid)
-	else:
-		print('Updating relation: '+narrower_uri+' "'+broader_label+'" (skos:narrower) "'+narrower_label+'"...')
-		lwb.updateclaim(broader_uri,"P73",narrower_uri,"item")
+    rowindex += 1
+    item = sparql.unpack_row(row, convert=None, convert_type={})
+    print('\nNow processing item [' + str(rowindex) + ']...')
+    print(str(item))
+    narrower_uri = item[0].replace("http://lexbib.elex.is/entity/", "")
+    narrower_label = item[1]
+    broader_uri = item[2].replace("http://lexbib.elex.is/entity/", "")
+    broader_label = item[3]
+    redirect_qid = item[4]
+    if redirect_qid != None:  # broader-rel points to redirect item
+        if redirect_qid.startswith("Q"):
+            print('Updating ' + broader_uri + ' redirect with ' + redirect_qid + '...')
+            broaderclaims = lwb.getclaims(narrower_uri, "P72")[1]
+            if "P72" in broaderclaims:
+                broaderstatement = broaderclaims['P72'][0]['id']
+                lwb.setclaimvalue(broaderstatement, redirect_qid, "item")
+                print('Updating relation: ' + narrower_uri + ' "' + str(broader_label) + '" (skos:narrower) "' + str(
+                    narrower_label) + '"...')
+                lwb.updateclaim(redirect_qid, "P73", narrower_uri, "item")
+        else:
+            print('Strange: for ' + broader_uri + ' I got redirect: ' + redirect_qid)
+    else:
+        print(
+            'Updating relation: ' + narrower_uri + ' "' + broader_label + '" (skos:narrower) "' + narrower_label + '"...')
+        lwb.updateclaim(broader_uri, "P73", narrower_uri, "item")
 
-
-print('\n\n________________________________________________________\n\n(2) get orphaned narrower relations (where the broader has been removed)...\n')
-query = config.lwb_prefixes+"""
+print(
+    '\n\n________________________________________________________\n\n(2) get orphaned narrower relations (where the broader has been removed)...\n')
+query = config.lwb_prefixes + """
 
 select distinct ?n ?nLabel ?narstatement ?b ?bLabel
 
@@ -92,20 +95,20 @@ print(query)
 
 url = "https://lexbib.elex.is/query/sparql"
 print("Waiting for SPARQL...")
-sparqlresults = sparql.query(url,query)
+sparqlresults = sparql.query(url, query)
 print('\nGot term list from LexBib SPARQL.')
 
-#go through sparqlresults
+# go through sparqlresults
 rowindex = 0
 
 for row in sparqlresults:
-	rowindex += 1
-	item = sparql.unpack_row(row, convert=None, convert_type={})
-	print('\nNow processing item ['+str(rowindex)+']\n')
-	narrower_uri = item[0].replace("http://lexbib.elex.is/entity/","")
-	narrower_label = item[1]
-	narstatement = item[2].replace("http://lexbib.elex.is/entity/statement/","")
-	broader_uri = item[3].replace("http://lexbib.elex.is/entity/","")
-	broader_label = item[4]
-	print('Removing orphaned narrower-rel: "'+broader_label+'" (skos:narrower) "'+narrower_label+'"...')
-	lwb.removeclaim(narstatement)
+    rowindex += 1
+    item = sparql.unpack_row(row, convert=None, convert_type={})
+    print('\nNow processing item [' + str(rowindex) + ']\n')
+    narrower_uri = item[0].replace("http://lexbib.elex.is/entity/", "")
+    narrower_label = item[1]
+    narstatement = item[2].replace("http://lexbib.elex.is/entity/statement/", "")
+    broader_uri = item[3].replace("http://lexbib.elex.is/entity/", "")
+    broader_label = item[4]
+    print('Removing orphaned narrower-rel: "' + broader_label + '" (skos:narrower) "' + narrower_label + '"...')
+    lwb.removeclaim(narstatement)
